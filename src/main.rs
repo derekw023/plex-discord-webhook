adf6839506c6165dc55055d36049fde2d2b72171
Derek Witt <derek@witt.me>
2022-01-10 23:50:20 -0500

    Initial scaffolding of passing data to discord

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use color_eyre::Report;
-use tracing::info;
+use tracing::{debug, error, info};
 use tracing_subscriber::EnvFilter;
 
+mod discord;
 mod plex;
 
 use std::sync::Arc;
@@ -10,19 +11,41 @@ use tokio::sync::Mutex;
 use warp::Filter;
 const MAX_LENGTH: u64 = 1024 * 1024;
 
+use plex::webhook::PlexWebhookRequest;
+
 #[tokio::main]
 async fn main() -> Result<(), Report> {
     setup()?;
 
-    let port: u16 = 8000;
+    let port: u16 = 8001;
+    let webhook_url: String = "DUMMY".to_string();
 
     let plex_handler = Arc::new(Mutex::new(plex::webhook::PlexHandler::new()));
+
+    let discord_client = Arc::new(discord::webhook::WebhookExecutor::new(webhook_url));
 
     let api = warp::path("plex")
         .and(warp::post())
         .map(move || plex_handler.clone())
         .and(warp::filters::multipart::form().max_length(MAX_LENGTH))
-        .and_then(plex::webhook::handle_webhook);
+        .and_then(plex::webhook::handle_webhook)
+        .map(move |msg| (msg, discord_client.clone()))
+        .then(
+            |arg: (PlexWebhookRequest, Arc<discord::webhook::WebhookExecutor>)| async move {
+                let msg = arg.0;
+                let client = arg.1;
+
+                let message = format!(
+                    "User {} {:?}'ed {}",
+                    msg.payload.account.title,
+                    msg.payload.event,
+                    msg.payload.metadata.unwrap().title.unwrap()
+                );
+
+                let content = discord::webhook::WebhookRequest { content: message };
+                client.clone().execute_webhook(content).await
+            },
+        );
 
     let server_future = warp::serve(api).run(([0, 0, 0, 0], port));
 
@@ -39,7 +62,10 @@ fn setup() -> Result<(), Report> {
     color_eyre::install()?;
 
     if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "info")
+        std::env::set_var(
+            "RUST_LOG",
+            "plex_discord_webhook=info,plex_discord_webhook::plex=debug",
+        )
     }
     tracing_subscriber::fmt::fmt()
         .with_env_filter(EnvFilter::from_default_env())
